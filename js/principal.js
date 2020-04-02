df6ef864d9b61e606bf0d25b5f0ba7e11568bcdb
Felipe Jatobá <jatobafcs@gmail.com>
2020-04-02 16:16:05 -0300

    New Layout Upload / index.html overwrite

---
+++ js/principal.js
@@ -0,0 +1,23 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function menuFunction() {
+    document.getElementById("dropdownMenu").classList.toggle("show");
+  }
+
+function searchFunction() {
+    document.getElementById("dropdownSearch").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content", "searchBar");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
